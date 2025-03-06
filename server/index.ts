105a47ff6652be88cbeeb665e9d883c56c968e5b
Nisila Perera <nisilainsitha1@gmail.com>
2025-03-06 12:47:48 +0530

    update server host to allow external connections

--- server/index.ts
+++ server/index.ts
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
   const port = process.env.PORT || 5000;
   server.listen({
     port,
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     backlog: 511,
   }, () => {
     log(`serving on port ${port}`);
